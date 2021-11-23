e6c6f70f315ac1c403a9bbe3c05bd6266e3ce577
Cristian Mayco da Costa <cristianmayco@gmail.com>
2021-11-23 14:53:50 -0300

    add swagger-ui and Open API

--- src/main/java/com/example/cambioservice/controller/CambioController.java
+++ src/main/java/com/example/cambioservice/controller/CambioController.java
@@ -2,6 +2,8 @@ package com.example.cambioservice.controller;
 
 import com.example.cambioservice.model.Cambio;
 import com.example.cambioservice.repository.CambioRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,7 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.List;
 
+@Tag(name="Cambio service API")
 @RestController
 @RequestMapping("cambio-service")
 public class CambioController {
@@ -23,6 +26,7 @@ public class CambioController {
     @Autowired
     private CambioRepository repository;
 
+    @Operation(description="Get cambio for currency")
     @GetMapping(value = "/{amount}/{from}/{to}")
     public Cambio getCambio(
             @PathVariable("amount") BigDecimal amount,
